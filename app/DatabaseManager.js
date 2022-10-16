a6ed55ef3fccf033208ecfa583ea2a57e038d573
sasame2032002 <sajjad.sama.sd@gmail.com>
2022-10-16 16:41:44 +0330

    show data in screen

---
+++ app/DatabaseManager.js
@@ -0,0 +1,7 @@
+export default getData = () => [
+  { id: 1, text: "todo 1", complate: false },
+  { id: 2, text: "todo 2", complate: false },
+  { id: 3, text: "todo 3", complate: false },
+  { id: 4, text: "todo 4", complate: false },
+  { id: 5, text: "todo 5", complate: false },
+];
--- app/MainScreen.js
+++ app/MainScreen.js
@@ -1,15 +1,39 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import getData from "./DatabaseManager";
+
+import { View, Text, StyleSheet, FlatList, StatusBar } from "react-native";
+import React, { useEffect, useState } from "react";
 
 export default function MainScreen() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(getData);
+  }, []);
+
   return (
     <View style={styles.center}>
-      <Text>Index</Text>
+      <StatusBar showHideTransition={true} />
+      <View style={styles.list}>
+        <FlatList
+          data={data}
+          renderItem={(item) => (
+            <View style={styles.items}>
+              <Text>{item.item.text}</Text>
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  items: {
+    paddingVertical: 15,
+  },
+  list: {
+    marginVertical: 20,
+  },
   center: {
     flex: 1,
     justifyContent: "center",
