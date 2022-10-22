a1635c1229544ad4e724593ab8dbe8293b2bd5c4
sasame2032002 <sajjad.sama.sd@gmail.com>
2022-10-22 06:16:37 +0330

    change color

--- app/MainScreen.js
+++ app/MainScreen.js
@@ -42,18 +42,13 @@ export default function MainScreen() {
             </View>
           )}
           renderItem={(item) => (
-            <Animated.View style={styles.items}>
+            <View style={styles.items}>
               <Text style={styles.itemsText}>{item.item.text}</Text>
-            </Animated.View>
+            </View>
           )}
           renderHiddenItem={() => (
             <View style={styles.hiddenItem}>
-              <Icon
-                name="trash"
-                color="#fff"
-                size={20}
-                style={styles.hiddenItemIcon}
-              />
+              <Icon name="trash" size={20} style={styles.hiddenItemIcon} />
             </View>
           )}
           rightOpenValue={-Dimensions.get("window").width}
@@ -75,6 +70,7 @@ export default function MainScreen() {
         <TextInput
           style={styles.newItem}
           placeholder="new task"
+          placeholderTextColor="#aaaa"
           value={newItem}
           onChangeText={(text) => setNewItem(text)}
           onSubmitEditing={() => {
@@ -94,33 +90,47 @@ const styles = StyleSheet.create({
   hiddenItemIcon: {
     justifyContent: "center",
     height: 25,
+    paddingRight: 10,
   },
   hiddenItem: {
-    backgroundColor: "red",
+    backgroundColor: "#7EC8E3",
     flex: 1,
     justifyContent: "center",
     alignItems: "flex-end",
     paddingHorizontal: 15,
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
   },
   newItem: {
     padding: 15,
     borderWidth: 1,
     borderRadius: 15,
     fontSize: 20,
+    borderColor: "#eee",
   },
   itemsText: {
     fontSize: 18,
+    color: "#eee",
   },
   items: {
     paddingVertical: 15,
-    backgroundColor: "white",
+    paddingHorizontal: 20,
+    backgroundColor: "#000C66",
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
   },
   list: {
     marginVertical: 20,
     width: "90%",
+    backgroundColor: "#000C66",
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+    borderBottomRightRadius: 15,
+    borderBottomLeftRadius: 15,
   },
   center: {
     flex: 1,
+    backgroundColor: "#0000FF",
     alignItems: "center",
   },
 });
